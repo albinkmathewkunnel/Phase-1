a52853c593c7c27539885583178e5d1aa0da39a3
albinkmathewkunnel <albinkmathew890@gmail.com>
2026-09-17 09:28:29 +0400

    Comment out maxofthree function and its test; retain findsum function implementation

--- functionmore.py
+++ functionmore.py
@@ -1,9 +1,19 @@
-def maxofthree(a,b,c):
-    if a>b and a>c:
-        return ("a",a) 
-    elif b>a and b>c:
-        return b
-    else:
-        return c
+# def maxofthree(a,b,c):
+#     if a>b and a>c:
+#         return ("a",a) 
+#     elif b>a and b>c:
+#         return b
+#     else:
+#         return c
 
-print(maxofthree(10,20,30))
+# print(maxofthree(10,20,30))
+
+list1 =[10,20,50]
+
+def findsum(list1):
+    sum=0
+    for i in list1:
+        sum+=i
+    return sum
+
+print(findsum(list1))
