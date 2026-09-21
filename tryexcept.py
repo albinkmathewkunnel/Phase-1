7f6d90cc224e65ef8d2f0fdbf1f5897cace05414
albinkmathewkunnel <albinkmathew890@gmail.com>
2026-09-21 16:05:30 +0400

    try except questions and answer

--- tryexcept.py
+++ tryexcept.py
@@ -11,34 +11,44 @@
 # except:
 #     print("an exception occured")
 
-try:
-    x=10/0
+# try:
+#     x=10/0
  
-    print("division by zero")
-finally:
-    print("executing finally block") 
+#     print("division by zero")
+# finally:
+#     print("executing finally block") 
 
 
-def safe_divide(a,b):
-    try:
-        return a/b
-    except ZeroDivisionError:
-        print("division by zero is not allowed")
-    except TypeError:
-        print("invalid input type")
+# def safe_divide(a,b):
+#     try:
+#         return a/b
+#     except ZeroDivisionError:
+#         print("division by zero is not allowed")
+#     except TypeError:
+#         print("invalid input type")
 
 
-safe_divide(10,0)
+# safe_divide(10,0)
 
 
-def read_file_content(filename):
-    try:
-        open.read(filename)
-    except FileNotFoundError:
-        print("file not found")
-    except IsADirectoryError:
-        print("expected file but found directory")
-    finally:
-        print("executing finally block")
+# def read_file_content(filename):
+#     try:
+#         open.read(filename)
+#     except FileNotFoundError:
+#         print("file not found")
+#     except IsADirectoryError:
+#         print("expected file but found directory")
+#     finally:
+#         print("executing finally block")
 
-read_file_content("test.txt")
+# read_file_content("test.txt")
+
+
+def calculate_mean(numbers):
+    if len(numbers)==0:
+        raise ValueError("The list is empty. Cannot calculate mean.")
+    return sum(numbers)/len(numbers)
+
+numbers=[1,2,3,4,5]
+
+print(calculate_mean(numbers))
